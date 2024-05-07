854b14322bdfdc886fbaa1872519381b9289248f
Замэ Максим Игоревич <MZame@alfabank.ru>
2024-05-07 18:45:11 +0300

    add config to exclude rules

--- src/main/java/custom/validation/openapi/Extensions.kt
+++ src/main/java/custom/validation/openapi/Extensions.kt
@@ -5,4 +5,10 @@ open class Extensions {
      * Параметр для указания пути до openapi контрактов
      */
     open var specPath: String = ""
+
+    /**
+     * Параметр для указания исключений правил для контракта
+     */
+    open var config: MutableMap<String, MutableMap<String, Boolean>> = mutableMapOf()
+
 }
