b7aef2a308890beaec93adb4566198129275e37f
Замэ Максим Игоревич <MZame@alfabank.ru>
2024-06-04 14:46:03 +0300

    updated rule checkRequiredFieldInModel

--- src/main/java/custom/validation/openapi/rules/models/fields/RequiredFieldInModel.kt
+++ src/main/java/custom/validation/openapi/rules/models/fields/RequiredFieldInModel.kt
@@ -7,16 +7,36 @@ import io.swagger.v3.oas.models.OpenAPI
  */
 class RequiredFieldInModel {
     fun checkRequiredFieldInModel(openAPI: OpenAPI): List<String> {
-        val missingRequiredFields: MutableList<String> = mutableListOf()
+        val missingRequiredFields = mutableListOf<String>()
+
         openAPI.components.schemas.forEach { (modelName, model) ->
+            // Собрать все свойства, включая allOf
+            val properties = model.properties?.toMutableMap() ?: mutableMapOf()
+            model.allOf?.forEach { schema ->
+                schema?.let {
+                    if (it.`$ref` != null) {
+                        val refModelName = it.`$ref`.substringAfterLast("/")
+                        openAPI.components.schemas[refModelName]?.properties?.let { refProperties ->
+                            properties.putAll(refProperties)
+                        }
+                    } else {
+                        it.properties?.let { inlineProperties ->
+                            properties.putAll(inlineProperties)
+                        }
+                    }
+                }
+            }
+
+            // Проверить наличие обязательных полей
             model.required?.forEach { requiredFieldName ->
-                if (!model.properties.containsKey(requiredFieldName)) {
+                if (!properties.containsKey(requiredFieldName)) {
                     missingRequiredFields.add(
                         "В модели \"$modelName\" отсутствует обязательное поле \"$requiredFieldName\""
                     )
                 }
             }
         }
+
         return missingRequiredFields
     }
 }
